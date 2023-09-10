7fa2485b51e1e07951843e16ed3750c4e4199f66
jagcity <devjag.linux@gmail.com>
2023-09-10 18:53:06 -0300

    changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,14 +11,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jagcity.github.io',
+  url: 'https://jagcity.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/darklab',
+  baseUrl: '/darklab/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'jagcity', // Usually your GitHub org/user name.
+  organizationName: 'jagcity', // Usually your GitHub org/username.
   projectName: 'darklab', // Usually your repo name.
 
   onBrokenLinks: 'throw',
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/darklab">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
