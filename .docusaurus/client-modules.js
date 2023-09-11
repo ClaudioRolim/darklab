f4d11667075ce809b4f6ec0c93e2495b34404af9
jagcity <devjag.linux@gmail.com>
2023-09-11 18:52:13 -0300

    Deploy website - based on 3527c5d35c83551f6eeb89e74cdbe368e58a5bbb

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/devjag/gitLocal/darklab/node_modules/infima/dist/css/default/default.css'),
-  require('/home/devjag/gitLocal/darklab/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/devjag/gitLocal/darklab/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/devjag/gitLocal/darklab/src/css/custom.css'),
-];
--- build/assets/js/18076591.71df599d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdarklab=self.webpackChunkdarklab||[]).push([[6816],{376:a=>{a.exports=JSON.parse('{"label":"docusaurus","permalink":"/darklab/blog/tags/docusaurus","allTagsPath":"/darklab/blog/tags","count":4}')}}]);
