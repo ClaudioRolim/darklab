a0bd3ffd288925a7265c1a3c601d679d8677226f
jagcity <devjag.linux@gmail.com>
2023-09-12 23:45:06 -0300

    ready project

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/devjag/gitLocal/darklab/node_modules/infima/dist/css/default/default.css'),
+  require('/home/devjag/gitLocal/darklab/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/devjag/gitLocal/darklab/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/devjag/gitLocal/darklab/src/css/custom.css'),
+];
